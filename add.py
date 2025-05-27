f5e5604d47dfa58c233d46ffc202c23a047563be
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-05-27 23:10:31 +0530

    addition of two variables

---
+++ add.py
@@ -0,0 +1,21 @@
+# pre-defined variables
+
+num1 = 20
+num2 = 30
+
+sum = num1 + num2
+
+print(sum)
+
+#or 
+
+print(num1+num2)
+
+# with user-inputs
+
+a = float(input("enter num1: "))
+b = float(input("enter num2: "))
+
+sum1 = a+b
+
+print(sum1)
