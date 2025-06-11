82e84e0f8d427353200ea09a0176e1e88853d796
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-06-11 23:08:40 +0530

    swap of two variables

---
+++ swap.py
@@ -0,0 +1,15 @@
+# python program to swap two variables
+print("before swap")
+x = 5
+y = 10
+print("x = ",x)
+print("y = ",y)
+
+#create a tempprary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print("after swap")
+print("x = ",x)
+print("y = ",y)
