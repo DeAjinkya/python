f349b5efd20c26acf82bfd88682872436e6e12bf
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-28 22:37:02 +0530

    variables in python

---
+++ variables/main.py
@@ -0,0 +1,26 @@
+age = 23 # int
+name = "Ajinkya" # str
+cgpa = 5.55 # float
+
+print(age)
+
+print(name)
+
+# 34age = 34 #SyntaxError: invalid decimal literal
+_ageq = 30
+age_3 = 23
+# age$$g = 45 #SyntaxError: invalid syntax
+
+# Python supports several built-in data types:
+# Integers ( int ): Whole numbers (e.g., 10 , -5 ).
+# Floats ( float ): Decimal numbers (e.g., 3.14 , -0.001 ).
+# Strings ( str ): Text data enclosed in quotes (e.g., "Hello" , 'Python' ).
+# Booleans ( bool ): Represents True or False .
+# Lists: Ordered, mutable collections (e.g., [1, 2, 3] ).
+# Tuples: Ordered, immutable collections (e.g., (1, 2, 3) ).
+# Sets: Unordered collections of unique elements (e.g., {1, 2, 3} ).
+# Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25} ).
+
+is_completed = True
+print(is_completed)
+print(type(is_completed))
