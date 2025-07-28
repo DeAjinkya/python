581f6a4c2401f222bf03de304bc452f3e6379153
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-28 23:22:02 +0530

    user input in python

---
+++ userinput/add.py
@@ -0,0 +1,16 @@
+a = int(input("enter a "))
+b = int(input("enter b "))
+print("a = ",a)
+print("b = ",b)
+
+c = a+b
+print("a+b = ",c)
+
+# str concatenation
+x = input("enter a ")
+y = input("enter b ")
+print("a = ",x)
+print("b = ",y)
+
+z = x+y
+print("a+b = ",z)
