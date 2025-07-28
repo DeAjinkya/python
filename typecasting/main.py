2a9dd68ba60bf3d3a5777fd325a1e6a1dceb431b
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-28 22:52:33 +0530

    typecasting in python

---
+++ typecasting/main.py
@@ -0,0 +1,17 @@
+a = 34
+b = "34"
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+# convert str into int
+c = int(b)
+print(c)
+print(type(c))
+
+# int to str
+d = str(b)
+print(d)
+print(type(d))
