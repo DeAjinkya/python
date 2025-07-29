9f8bc4ce33929c0300b8a4832df6bafc0359907e
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-29 19:25:26 +0530

    loops in python

---
+++ loops/main.py
@@ -0,0 +1,15 @@
+age = 18
+
+# if - else 
+if (age>18):
+    print("you can vote")
+else:
+    print("u can't vote")
+
+# if - elif - else
+if (age>18):
+    print("you can vote")
+elif (age == 18):
+    print("you can vote")
+else:
+    print("no vote")
