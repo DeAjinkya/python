039ac8582d38221d982670889df5dba7ab9d215a
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-30 16:24:00 +0530

    lambda function in python

---
+++ functions/lambda.py
@@ -0,0 +1,14 @@
+# lambda function
+"""
+def square(x):
+    return x*x
+"""
+square = lambda x:x*x
+print(square(2))
+
+"""
+def sum(x,y):
+    return x+y
+"""
+sum = lambda x,y:x+y
+print(sum(3,4))
