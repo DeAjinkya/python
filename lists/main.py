be981da45a3c6d65db7d52cfc7c4dbc47036f746
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-31 00:15:03 +0530

    list in python

---
+++ lists/main.py
@@ -0,0 +1,11 @@
+#list
+
+marks = [33,45,56,67,54]
+
+print(marks[0]) #33
+print(marks[0:4]) #[33, 45, 56, 67]
+
+mixed = [23.33,"ajinkya",False,45]
+
+print(mixed[0]) # 23.33
+print(mixed[0:3]) # [23.33, 'ajinkya', False]
