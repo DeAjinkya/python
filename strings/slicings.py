5e9a82cb993d31e9b205d1c2dede5a07ed3982f6
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-30 11:50:53 +0530

    string slicing in python

---
+++ strings/slicings.py
@@ -0,0 +1,11 @@
+a  = "Ajinkya is good coder"
+
+print(a[0:2]) # 0 and 1 index are shown
+print(a[2:-1]) 
+
+name = "Ajinkya"
+print(name[0:7:1]) # skips 0 character
+print(name[0:7:2]) # skips 1 character
+
+print(name[:4]) # from 0 to 4
+print(name[4:]) # from 4 to end or last (length - 4)
