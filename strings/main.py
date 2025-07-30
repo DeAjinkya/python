44065b1dd5f039692f9afa75fe46bd97571e3651
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-30 11:36:04 +0530

    strings in python

---
+++ strings/main.py
@@ -0,0 +1,34 @@
+name = "ajinkya"
+
+a = 'ajinkya'
+
+# multi-line string
+b = """ajinkya
+is a good developer"""
+
+print(name)
+print(a)
+print(b)
+
+#       -7 -6 -5 -4 -3 -2 -1
+# name = a  j  i  n  k  y  a
+#        0  1. 2. 3. 4. 5  6. 
+
+#indexing +
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+# print(name[6])
+# print(name[7]) #IndexError: string index out of range
+
+#indexing -
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+print(name[-6])
+print(name[-7])
