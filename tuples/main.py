669559fd6cfccdf68feef4ea779dfcb0c657b187
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-31 01:01:56 +0530

    tuples in python

---
+++ tuples/main.py
@@ -0,0 +1,13 @@
+# tuple
+
+my_tuple = (1,2,3,4,5)
+
+print(my_tuple[0])
+print(my_tuple[0:3])
+
+#single element tuple
+a = (3,)
+b = (3)
+
+print(type(a))
+print(type(b))
---
+++ tuples/unpack.py
@@ -0,0 +1,6 @@
+# tuple unpacking
+
+my_tuple = [10,20,30]
+
+a,b,c = my_tuple
+print(a,b,c)
