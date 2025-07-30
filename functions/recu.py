a84a88a22206b6ae1e8a0dd60c5b425308abe799
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-30 17:09:10 +0530

    recursion in python

--- functions/recu.py
+++ functions/recu.py
@@ -9,3 +9,10 @@ fib(2)= fib(0) + fib(1)
 fib(n)= fib(n-2) + fib(n-1)
 """
 
+def fib(n):
+    # base case of recursion
+    if (n==0 or n==1):
+        return n
+    return fib(n-2) + fib(n-1)
+
+print(fib(6)) # 8 (6 is index)
