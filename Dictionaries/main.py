98ac3a21bce8ad4e2074252a9fd6ea93d1761d66
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-07-31 13:34:15 +0530

    dictionary in python

---
+++ Dictionaries/main.py
@@ -0,0 +1,15 @@
+# Dictionary
+
+marks ={ 
+    "Ajinkya":98,
+    "jack":67,
+    "lily":55
+}
+
+print(marks["Ajinkya"]) # 98
+
+marks["jack"] = 78
+print(marks) #{'Ajinkya': 98, 'jack': 78, 'lily': 55}
+
+marks["raj"] = 65
+print(marks) # {'Ajinkya': 98, 'jack': 78, 'lily': 55, 'raj': 65}
