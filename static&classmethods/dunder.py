0f5b5e292af2298e870c304f5fcc5271c25f1cd7
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-08-01 13:39:28 +0530

    dunder/magic methods

---
+++ static&classmethods/dunder.py
@@ -0,0 +1,20 @@
+class Employee:
+    company = "google"
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+    def __str__(self):
+        return f"the name is {self.name} and salary is {self.salary}"
+    
+    def __repr__(self):
+        return f"name: {self.name}, salary: {self.salary}"
+    
+    def __len__(self):
+        return len(self.name)
+
+e = Employee("raj",50000)
+print(e.name,e.salary)
+print(str(e))
+print(repr(e))
+print(len(e))
