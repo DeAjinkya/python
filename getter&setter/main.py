e9a95d8cf639ee2e226c85f231a58ae4cffc48c7
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-08-01 12:48:12 +0530

    getter and setter

---
+++ getter&setter/main.py
@@ -0,0 +1,22 @@
+# getter and setter
+
+class Employee:
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+    @property
+    def first_name(self):
+        l = self.name.split(" ")
+        return l[0]
+    
+    @first_name.setter
+    def first_name(self,first):
+        l = self.name.split(" ")
+        new_name = f"{first} {l[1]}"
+        self.name = new_name
+
+e = Employee("john doe",10000)
+print(e.first_name)
+e.first_name = "jack"
+print(e.name)
