3c6e1191a9c9820dee04874375f4e89b73907b0c
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-08-03 15:19:03 +0530

    finally in try catch

---
+++ exceptionhandling/finally.py
@@ -0,0 +1,14 @@
+# finally
+
+try:
+    a = 34/0
+
+except Exception as e:
+    print(e)
+
+# else part will run when no error in try code
+else:
+    print("hiii")
+
+finally:
+    print("always executed")
