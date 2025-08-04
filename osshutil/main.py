350aca0e6887ec5d769695862cd814c0027f6d32
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-08-04 23:56:58 +0530

    os module

---
+++ osshutil/main.py
@@ -0,0 +1,14 @@
+import os
+
+a = os.listdir("new")
+print(a)
+
+# current working directory
+print(os.getcwd())
+
+print(os.path.exists("new")) # returns true if present 
+
+# os.remove("sample.txt") # remove a file
+
+# os.rmdir("dir") # removes a directory
+
