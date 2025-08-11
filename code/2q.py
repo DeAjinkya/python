a8b3d6afc03eaef65d594f2f570a46f98d729bb5
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-08-11 19:31:55 +0530

    arithmetic operators

---
+++ code/2q.py
@@ -0,0 +1,25 @@
+# arithmetic operators
+
+x = 15
+y = 4
+
+print('x + y =', x+y)
+
+print('x - y =', x-y)
+
+print('x x y =', x*y)
+
+print('x / y =', x/y)
+
+print('x // y =', x//y)
+
+print('x ** y =', x**y)
+
+"""
+x + y = 19
+x - y = 11
+x x y = 60
+x / y = 3.75
+x // y = 3
+x ** y = 50625
+"""
