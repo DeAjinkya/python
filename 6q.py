6a32625f1211a54802d382dcf18f2dd9c9a8ad6a
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-08-18 22:21:54 +0530

    Update 6q.py

--- 6q.py
+++ 6q.py
@@ -1,10 +1,10 @@
 # Program to check whether a number is even or odd
 
-number = int(input("Enter a number: ")) # getting a number input to check for odd or even 
-
+# number = int(input("Enter a number: ")) # getting a number input to check for odd or even 
+num = 10
 # checking number using the remainder
 
-if number % 2 == 0:
+if num % 2 == 0:
     print("The number is Even") # Even no if remainder is 0
 else:
     print("The number is Odd") # Odd no if remainder is not 0
